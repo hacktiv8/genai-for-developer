3a13fc6d78ad1a6fb22537aa5ee5431f3d0ef210
Riza Fahmi <rizafahmi@gmail.com>
2024-06-08 09:15:12 +0700

    Init

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A Nix-flake-based Node.js development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self , nixpkgs ,... }: let
+    # system should match the system you are running on
+    # system = "x86_64-linux";
+    system = "aarch64-darwin";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      # create an environment with nodejs_18
+      packages = with pkgs; [
+        nodejs_22
+      ];
+
+      shellHook = ''
+        echo "node `${pkgs.nodejs}/bin/node --version`"
+      '';
+    };
+  };
+}
