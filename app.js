64e971e82c38f7e8e2a0ae9220a55d00384d3d67
Riza Fahmi <rizafahmi@gmail.com>
2024-06-09 13:27:34 +0700

    Add query string

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const app = new Hono();
 app.get("/health", function (ctx) {
 	return ctx.text("OK");
 });
+app.get("/chat", async function (ctx) {
+	const inquiry = ctx.req.query("q");
+	// const response = await generate(prompt);
+	return ctx.text(inquiry);
+});
 app.get("/", async function (ctx) {
 	return ctx.html(fs.readFileSync("index.html"));
 });
