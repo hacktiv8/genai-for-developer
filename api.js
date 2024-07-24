4110aef1571a035327e5f3a094e67d557b2b3b1d
Riza Fahmi <rizafahmi@gmail.com>
2024-07-24 07:42:03 +0700

    Use structured output parser with zod

--- api.js
+++ api.js
@@ -1,29 +1,34 @@
 import { Hono } from "hono";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
-import { ChatOllama } from "@langchain/community/chat_models/ollama";
+import { Ollama } from "@langchain/community/llms/ollama";
 import { StructuredOutputParser } from "@langchain/core/output_parsers";
+import { OutputFixingParser } from "langchain/output_parsers";
+import { z } from "zod";
 
 const api = new Hono();
-const model = new ChatOllama({
+const model = new Ollama({
   model: "mistral-openorca",
   temperature: 0,
   top_k: 12,
-  format: "json",
 });
 
 const prompt = ChatPromptTemplate.fromMessages([
   ["system", "You are great and creative at finding food recipes."],
   [
     "human",
-    "Please provide one or more recipe and it's variants for {query} in valid JSON format.",
+    "Please provide one or more recipe and it's variants for {query}. Format Instructions: {format_instructions}",
   ],
 ]);
 
-const outputParser = StructuredOutputParser.fromNamesAndDescriptions({
-  name: "The name of the recipe",
-  ingredients: "The ingredients required for the recipe",
-  instructions: "The instructions to prepare the recipe",
-});
+const outputParser = StructuredOutputParser.fromZodSchema(
+  z.array(
+    z.object({
+      name: z.string().describe("Name of the recipe"),
+      ingredients: z.array(z.string().describe("Ingredients of the recipe")),
+      instructions: z.array(z.string().describe("Instructions of the recipe")),
+    }),
+  ),
+);
 
 // prompt |> model |> parse |> invoke
 const chains = prompt.pipe(model).pipe(outputParser);
@@ -31,11 +36,20 @@ const chains = prompt.pipe(model).pipe(outputParser);
 // Recipe Service
 api.get("/api/recipe", async function (ctx) {
   const query = ctx.req.query("q");
-  const response = await chains.invoke({ query: query });
-  console.log(response);
+  const res = await chains.invoke({
+    query: query,
+    format_instructions: outputParser.getFormatInstructions(),
+  });
+  let response;
+  if (typeof response === "string") {
+    response = JSON.parse(res);
+  } else {
+    response = res;
+  }
+
   return ctx.json({
     status: "OK",
-    response: JSON.parse(response),
+    response,
     query,
   });
 });
